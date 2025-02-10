052c6a07bd033cb90ef3b5eb3959cc549f49a82e
Celso Pires <celso.pires1999@gmail.com>
2025-02-09 22:54:05 -0300

    pedropark99

--- event-of-code/day01/sort_example.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-pub fn main() void {
-    var items: [4]u8 = .{ 3, 2, 2, 1 };
-    std.sort.block(u8, &items, {}, std.sort.asc(u8));
-    std.debug.print("{any}\n", .{items});
-}
